fb950e9dee1bce0a23b59e3ed35e92fda937a2f5
Борис Кебаб <levon44432@gmail.com>
2024-05-29 11:25:09 +0300

    Added models for client and founder

---
+++ TeledocTest.Core/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿using TeledocTest.Core.Enums;
+
+namespace TeledocTest.Core.Models;
+public class Client : BaseEntity
+{
+    public string TaxID { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public ClientType Type { get; set; }
+}
